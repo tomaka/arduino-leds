f2b836a4902a9518d83ea522424993295fec9041
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-08-30 17:27:45 +0200

    Well, this is supposed to work

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,59 @@
 #![no_std]
 #![no_main]
 
-use ruduino::Pin;
-use ruduino::cores::current::{port};
+use ruduino::Pin as _;
+use ruduino::cores::atmega328::port;
 
 #[no_mangle]
-pub extern fn main() {
-    port::B5::set_output();
+pub extern "C" fn main() {
+    port::B3::set_output();
 
     loop {
-        port::B5::set_high();
+        ruduino::interrupt::without_interrupts(|| {
+            for _ in 0..50 {  // Num LEDs
+                send_byte(255);
+                send_byte(255);
+                send_byte(0);
+            }
+        });
 
-        ruduino::delay::delay_ms(1000);
-
-        port::B5::set_low();
-
-        ruduino::delay::delay_ms(100);
+        port::B3::set_low();
+        ruduino::delay::delay_us(50);
     }
+}
+
+#[inline(always)]
+fn send_byte(byte: u8) {
+    for n in 0..8 {
+        if (byte & (1 << n)) == 1 {
+            send1();
+        } else {
+            send0();
+        }
+    }
+}
+
+#[inline(always)]
+fn send0() {
+    port::B3::set_high();
+    delay_ns(500);
+    port::B3::set_low();
+    delay_ns(2000);
+}
+
+#[inline(always)]
+fn send1() {
+    port::B3::set_high();
+    delay_ns(1200);
+    port::B3::set_low();
+    delay_ns(1300);
+}
+
+#[inline(always)]
+fn delay_ns(ns: u32) {
+    // Note: the division by 4 is done because each loop passed to `delay` is 4 cycles.
+    // See also <https://docs.rs/avr_delay/0.3.1/src/avr_delay/lib.rs.html#27-47>
+    let ns_lp = 1000000000 / (ruduino::config::CPU_FREQUENCY_HZ / 4);
+    let loops = (ns / ns_lp) as u32;
+    ruduino::delay::delay(u64::from(loops));
 }
