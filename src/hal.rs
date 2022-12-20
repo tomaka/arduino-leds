a743409e182ca206c343d57d822891b1df7abac0
Pierre Krieger <pierre.krieger1708@gmail.com>
2022-12-20 12:19:15 +0100

    Docs

--- src/hal.rs
+++ src/hal.rs
@@ -23,6 +23,7 @@ pub fn upload_bport_data<const PIN: usize>(input_data: &[u8]) {
     unsafe {
         // See <http://ww1.microchip.com/downloads/en/devicedoc/atmel-0856-avr-instruction-set-manual.pdf>
         // and <https://github.com/rust-lang/rust/blob/263d8682d6e01bb02727b15b1c72ffabc0e7396b/compiler/rustc_target/src/asm/avr.rs>
+        // For reminder, 1 cycle = 62.5ns
 
         // To write a 1, we set the bit high for 10 cycles (1125ns) then low for 4 cycles (250ns).
         // To write a 0, we set the bit high for 4 cycles (250ns) then low for 10 cycles (1125ns).
@@ -48,9 +49,11 @@ pub fn upload_bport_data<const PIN: usize>(input_data: &[u8]) {
                 cbi {addr}, {pin}       // 2 cycles
 
                 // T= 7 (if bit 7 of `val` was set) or 8 cycles (if bit 7 of `val` was clear)
-                dec {nbits}             // T= 7 or 8 (depending on whether bit 7 of val was set)
-                breq 1f                 // T= 8 or 9
+                dec {nbits}             // 1 cycle
+                // Jump to label `1` if `nbits` is now 0
+                breq 1f                 // 1 cycle if condition is false, 2 if true
 
+                // T= 9 or 10 (depending on bit 7 of `val`)
                 nop
                 nop
                 nop
@@ -60,33 +63,47 @@ pub fn upload_bport_data<const PIN: usize>(input_data: &[u8]) {
                 nop
                 nop
                 nop
-                sbrc {val}, 7           // T= 18 or 19, skip next instruction if bit 7 of val is clear
-                cbi {addr}, {pin}       // set pin output to 0
 
-                rol {val}               // T= 21, rotate the value to write so that bit 7 becomes bit 6
+                // T= 18 or 19
+                // Set pin output to 0 if bit 7 of `val` is set
+                // Either this `cbi` or the one above is executed, but never both
+                sbrc {val}, 7
+                cbi {addr}, {pin}
+
+                // T= 21
+                rol {val}               // 1 cycle
                 nop
-                rjmp 0b                 // T= 23
+                rjmp 0b                 // 2 cycles
+                // We jump back to label `0` at T= 25
 
             1:
-                ldi {nbits}, 8          // T= 10 or 11, reset nbits to 8
-                dec {nbytes_low}        // T= 11 or 12, we update nbytes_low for the byte we've just sent
-                breq 2f                 // T= 12 or 13
-                ld {tmp}, X+            // T= 13 or 14, load the next byte to write
+                // We arrive here at T= 10 or 11
+                // Reset nbits to 8
+                ldi {nbits}, 8          // 1 cycle
+                dec {nbytes_low}
+                // T= 12 or 13
+                breq 2f                 // 1 cycle if condition is false, 2 if true
+                ld {tmp}, X+            // 1 cycle, although it's kind of complicated
 
+                // T= 14 or 15
                 nop
                 nop
                 nop
                 nop
-                sbrc {val}, 7           // T= 18 or 19, skip next instruction if bit 7 of val is clear
-                cbi {addr}, {pin}       // set pin output to 0
+                sbrc {val}, 7
+                cbi {addr}, {pin}
 
-                mov {val}, {tmp}        // T= 21
+                // T= 21
+                mov {val}, {tmp}        // 1 cycle
                 nop
-                brne 0b                 // T= 23
+                rjmp 0b                 // 2 cycles
+                // We jump back to label `0` at T= 25
 
             2:
-                dec {nbytes_high}       // T= 14 or 15
-                breq 3f                 // T= 15 or 16, jump to the end of no more data
+                // We arrive here at T= 14 or 15
+                dec {nbytes_high}
+                // Jump to the end if no more data
+                breq 3f
                 ldi {nbytes_low}, 0     // T= 16 or 17, reset nbytes_low
                 ld {tmp}, X+            // T= 17 or 18, load the next byte to write
 
@@ -96,15 +113,14 @@ pub fn upload_bport_data<const PIN: usize>(input_data: &[u8]) {
                 mov {val}, {tmp}        // T= 21
                 nop
                 rjmp 0b                 // T= 23
+                // We jump back to label `0` at T= 25
 
             3:
                 nop
                 nop
                 cbi {addr}, {pin}       // set pin output to 0
-                nop
-                nop
-                nop
 
+                // Trailer to restore the SREG value.
                 pop {tmp}
                 sts 0x5f, {tmp}     // SREG
             "#,
