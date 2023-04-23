f2a7a156f062d5b94ea48f6cb8904b2814904e4d
Pierre Krieger <pierre.krieger1708@gmail.com>
2023-04-23 12:40:56 +0200

    Pin the toolchchain

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-AVR_CPU_FREQUENCY_HZ=16000000 cargo +nightly build -Z build-std-features=compiler-builtins-mem -Z build-std=core,alloc --target avr-unknown-gnu-atmega328p.json --release
+AVR_CPU_FREQUENCY_HZ=16000000 cargo +nightly-2023-03-25 build -Z build-std-features=compiler-builtins-mem -Z build-std=core,alloc --target avr-unknown-gnu-atmega328p.json --release
